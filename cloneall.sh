324234cccced0bb1a9bda7aa413f6673f81b2022
fxh416 <fxh416@cs.bham.ac.uk>
2018-03-27 02:40:28 +0100

    auto

--- cloneall.sh
+++ cloneall.sh
@@ -1,4 +1,4 @@
-exec `cp ~/.bashrc ./`
+exec `cp ~/.bashrc ./bashrc`
 exec `cp -r ~/.config/redshift.conf ./config/redshift.conf`
 exec `cp -r ~/.config/sublime-text-3/ ./config/sublime-text-3/`
 exec `git add *`
